6b1cdb51cbcd689cede0fc5759e9fdc45ad462ea
ovaskann <ovaskainen.annasofia@gmail.com>
2026-04-01 18:00:49 +0300

    Add routes

---
+++ src/data/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        id: 1,
+        question: "What is the capital of Finland?",
+        answer: "Helsinki",
+        keywords: ["geography", "capital"]
+    },
+    {
+        id: 2,
+        question: "What is the capital of Norway?",
+        answer: "Oslo",
+        keywords: ["geography", "capital"]
+    },
+    {
+        id: 3,
+        question: "Who composed Für Elise?",
+        answer: "Ludwig van Beethoven",
+        keywords: ["music", "composer"]
+    },
+    {
+        id: 4,
+        question: "Which scientist developed the theory of relativity?",
+        answer: "Albert Einstein",
+        keywords: ["science", "physics"]
+    },
+];
+module.exports = questions;
